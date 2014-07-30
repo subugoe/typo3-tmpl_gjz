8e2b5e8d63d8257fedf043d677503943d77765e4
Ingo Pfennigstorf <i.pfennigstorf@gmail.com>
2014-07-30 09:01:43 +0200

    Initial import

---
+++ ext_emconf.php
@@ -0,0 +1,35 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = array (
+	'title' => 'GJZ Template',
+	'description' => 'Template and Configuration for Gelehrte Journale Website',
+	'category' => 'plugin',
+	'shy' => 0,
+	'version' => '1.0.0',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'state' => 'beta',
+	'uploadfolder' => 1,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearcacheonload' => 1,
+	'lockType' => '',
+	'author' => 'Marcus Hellmann',
+	'author_email' => 'marcus.hellmann@mail.uni-goettingen.de',
+	'author_company' => '',
+	'constraints' =>
+	array (
+		'depends' =>
+		array (
+			'typo3' => '6.2.0-6.2.99',
+		),
+		'conflicts' => '',
+		'suggests' =>
+		array (
+		),
+	),
+);
+
+?>
