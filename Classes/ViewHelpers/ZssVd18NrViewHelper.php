4c11908fc94675b66f0e3f49bf316317a0f90df4
Marcus Hellmann <marcus.hellmann.bsc@gmail.com>
2015-05-11 13:27:08 +0200

    Create ZssVd18NrViewHelper.php

---
+++ Classes/ViewHelpers/ZssVd18NrViewHelper.php
@@ -0,0 +1,66 @@
+<?php
+/*******************************************************************************
+ * Copyright notice
+ *
+ * Copyright 2015 Marcus Hellmann, Göttingen Academy of Sciences and Humanities
+ *                <mhellma1@gwdg.de>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ ******************************************************************************/
+
+
+/**
+ * View Helper to return the VD18-No. of the journal
+ */
+namespace Gjz18\TmplGjz\ViewHelpers;
+class ZssVd18NrViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper {
+
+
+	/**
+	 * Registers own arguments.
+	 */
+	public function initializeArguments() {
+		parent::initializeArguments();
+		$this->registerArgument('array', 'array', 'The array to extract the values from', TRUE);
+	}
+
+
+	/**
+	 * @return string
+	 */
+	public function render() {
+		$result = NULL;
+    $final = NULL;
+
+		if ($this->arguments['array']) {
+			$result = $this->arguments['array'];
+		}
+    
+    if ( strpos($result[0], "VD18")!==FALSE ) {
+      $final = (string)str_replace("VD18 ", "", $result[0]);
+      $final = (string)str_replace("VD18", "", $final);
+      $final = (string)str_replace(" ", "", $final);
+    }
+    return $final;
+    
+	}
+
+}
+
+?>
