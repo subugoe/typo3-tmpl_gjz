56cf61c5f8375d664f2e1cade401d77d94202d6d
Marcus Hellmann <marcus.hellmann.bsc@gmail.com>
2015-05-13 15:39:08 +0200

    Update ZssStatistikSystemstellenViewHelper.php

--- Classes/ViewHelpers/ZssStatistikSystemstellenViewHelper.php
+++ Classes/ViewHelpers/ZssStatistikSystemstellenViewHelper.php
@@ -62,57 +62,14 @@ class ZssStatistikSystemstellenViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelp
     
     
     $url = "http://gjz18solr.tc.sub.uni-goettingen.de/solr-adw/adw/select?q=d039Bs9%3A".$idClean."&rows=1&fl=d045Q01s9&wt=xml&indent=true&facet=true&facet.query=d039Bs9%3A".$idClean."&facet.field=d045Q01s9";
-    $useragent="cURL";
-    $headers=false;
-    $follow_redirects=false;
-    $debug=false;
+    $includeHeader=false;
+    $requestHeaders=false;
+    $report=NULL;
     
-    # initialise the CURL library
-    $ch = curl_init();
-     
-    # specify the URL to be retrieved
-    curl_setopt($ch, CURLOPT_URL,$url);
-     
-    # we want to get the contents of the URL and store it in a variable
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
-     
-    # specify the useragent: this is a required courtesy to site owners
-    curl_setopt($ch, CURLOPT_USERAGENT, $useragent);
-     
-    # ignore SSL errors
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-     
-    # return headers as requested
-    if ($headers==true){
-    curl_setopt($ch, CURLOPT_HEADER,1);
-    }
-     
-    # only return headers
-    if ($headers=='headers only') {
-    curl_setopt($ch, CURLOPT_NOBODY ,1);
-    }
-     
-    # follow redirects - note this is disabled by default in most PHP installs from 4.4.4 up
-    if ($follow_redirects==true) {
-    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1); 
-    }
-     
-    # if debugging, return an array with CURL's debug info and the URL contents
-    if ($debug==true) {
-    $result['contents']=curl_exec($ch);
-    $result['info']=curl_getinfo($ch);
-    }
-     
-    # otherwise just return the contents as a variable
-    else $xml=curl_exec($ch);
-     
-    # free resources
-    curl_close($ch);
-    
-    
+    $xml = GeneralUtility::getUrl($url, $includeHeader, $requestHeaders, $report);
     
     if ($xml) {
-      $sxe = new SimpleXMLElement($xml);
+      $sxe = new \SimpleXMLElement($xml);
       
       if ($sxe) {
         $zssArrayRaw_SystStPPN = $sxe->xpath("//lst[@name='d045Q01s9']/int/@name");
