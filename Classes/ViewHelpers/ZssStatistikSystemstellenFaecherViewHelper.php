a3828df11301e571ebfbf0899ebc5ca74739e825
Marcus Hellmann <mhellma1@gwdg.de>
2015-06-25 14:15:02 +0200

    Added Solr queries for improved classification statistics plot

--- Classes/ViewHelpers/ZssStatistikSystemstellenFaecherViewHelper.php
+++ Classes/ViewHelpers/ZssStatistikSystemstellenFaecherViewHelper.php
@@ -41,6 +41,8 @@ class ZssStatistikSystemstellenFaecherViewHelper extends \TYPO3\CMS\Fluid\Core\V
     parent::initializeArguments();
     $this->registerArgument('string', 'string', 'The array to extract the type from', TRUE);
     $this->registerArgument('decision', 'integer', 'The array to extract the title from', TRUE);
+    $this->registerArgument('beginn', 'int', 'The int to extract the year of publication begin from');
+    $this->registerArgument('ende', 'int', 'The int to extract the year of publication end from');
   }
 
 
@@ -90,6 +92,65 @@ class ZssStatistikSystemstellenFaecherViewHelper extends \TYPO3\CMS\Fluid\Core\V
       }
       
       return $faecherCountArray;
+      
+    }
+    
+    elseif ($key == 3) {
+      $id = NULL;
+      $url = NULL;
+      $includeHeader = false;
+      $requestHeaders = false;
+      $report = NULL;
+      $xml = NULL;
+      $sxe = NULL;
+      $faecherCountArray = array();
+      
+      if ($this->arguments['string']) {
+        $id = $this->arguments['string'];
+      }
+      $idClean = str_replace(" ", "", $id[0]);
+      
+      /** @years : length of publication period */
+      if ($this->arguments['beginn']) {
+        $pubBegin = $this->arguments['beginn'];
+      }
+      if ($this->arguments['ende']) {
+        $pubEnd = $this->arguments['ende'];
+      }
+      $years = ($pubEnd - $pubBegin) + 1;
+      
+      $faecher = array("WISSENSCHAFTSKUNDE", "PHILOLOGIE", "THEOLOGIE", "JURISPRUDENZ", "MEDIZIN", "PHILOSOPHIE", "P*DAGOGIK", "STAATSWISSENSCHAFTEN", "KRIEGSWISSENSCHAFTEN", "NATURKUNDE", "TECHNOLOGIE", "MATHEMATIK", "GEOGRAPHIE", "GESCHICHTE", "BILDENDE", "LITER*R", "VERMISCHTE");
+      $faecherClean = array("Wissenschaftskunde", "Philologie", "Theologie", "Jurisprudenz", "Medizin und Pharmazie", "Philosophie", "Pädagogik", "Staatswissenschaften", "Kriegswissenschaften", "Naturkunde", "Technologie und Gewerbekunde", "Mathematik", "Geographie", "Geschichte", "Bildende Kunst, schöne Literatur, Musik", "Literär-/Gelehrtengeschichte", "Vermischte Schriften");
+      $faecherCleanAbbrevs = array("Wiss.", "Philol.", "Theol.", "Jur.", "Med.", "Phil.", "Päd.", "Pol.", "Mil.", "Nat.", "Tech.", "Math.", "Geo.", "Gesch.", "Kü.", "Lit.", "Verm.");
+      $faecherCountArrayDim2 = array_fill(0, 16, "");
+      $faecherCountArrayDim3 = array_fill(0, $years, "");
+      
+      for ($y=0; $y<$years; $y++) {
+        
+        $year=$pubBegin+$y;
+        
+        for ($i=0; $i<count($faecher); $i++) {
+          $url = "http://134.76.20.176:8080/solr/adw/select?q=d039Bs9%3A".$idClean."&fq=i011_sa%3A%22".$year."%22+d045Q01sa%3A".$faecher[$i]."*&rows=0&fl=d045Q01sa&wt=xml&indent=true";
+          $xml = t3lib_div::getUrl($url, $includeHeader, $requestHeaders, $report);
+          if ($xml==NULL) {
+            $fachCount[0] = 0;
+          }
+          else {
+            $sxe = new \SimpleXMLElement($xml);
+            $fachCount = $sxe->xpath("//response/result[@name='response']/@numFound");
+          }
+          $faecherCountArray[$i] = $fachCount[0];
+        }
+        
+        for ($c=0; $c<17; $c++) {
+          $faecherCountArrayDim2[$c] = [ $faecherCleanAbbrevs[$c], $faecherCountArray[$c] ];
+        }
+        
+        $faecherCountArrayDim3[$y] = $faecherCountArrayDim2;
+        
+      }
+      
+      return $faecherCountArrayDim3;
       
     }
     
