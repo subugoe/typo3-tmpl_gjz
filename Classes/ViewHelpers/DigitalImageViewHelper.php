b971fa3ca2671b09ec2008759c046f5ba0e65ef1
mh-goe <marcus.hellmann.bsc@gmail.com>
2015-12-11 19:19:24 +0100

    Testing

--- Classes/ViewHelpers/DigitalImageViewHelper.php
+++ Classes/ViewHelpers/DigitalImageViewHelper.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace Gjz18\TmplGjz\ViewHelpers;
-//use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Core\Imaging\GraphicalFunctions;
 
 class DigitalImageViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper {
@@ -88,11 +88,11 @@ class DigitalImageViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelper\AbstractVi
       $resultURL .= ".jpg";
       if(!@GraphicalFunctions::getImageDimensions($resultURL)) {
         $resultURL = "1";
-        /*$includeHeader="1";
+        $includeHeader="1";
         $requestHeaders=false;
         $report=NULL;
-        $html = GeneralUtility::getUrl($url, $includeHeader, $requestHeaders, $report);*/
-      /*$ch = curl_init();
+        $html = GeneralUtility::getUrl($url, $includeHeader, $requestHeaders, $report);
+        $ch = curl_init();
         $timeout = 0;
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
@@ -105,7 +105,7 @@ class DigitalImageViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelper\AbstractVi
           if (strpos($imageLink->getAttribute('src'), "PPN") !== FALSE) {
             $resultURL = $imageLink->getAttribute('src');
           }
-        }*/
+        }
       }
       return $resultURL;
     }
