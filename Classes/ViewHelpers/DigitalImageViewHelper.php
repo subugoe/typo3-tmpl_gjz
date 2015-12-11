3a47cc8ffdb5761cc84c7b34f449b75f29993ffe
mh-goe <marcus.hellmann.bsc@gmail.com>
2015-12-11 18:23:19 +0100

    Testing + echo

--- Classes/ViewHelpers/DigitalImageViewHelper.php
+++ Classes/ViewHelpers/DigitalImageViewHelper.php
@@ -108,6 +108,7 @@ class DigitalImageViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelper\AbstractVi
         /** Get image tags */
         $tag = "img";
         foreach($dom->HtmlParser::get_tag_attributes($tag) as $imageLink) {
+          echo($imageLink);
           /*if (strpos($imageLink, "PPN") !== FALSE) {
             $resultURL = $imageLink->getAttribute('src');
           }*/
