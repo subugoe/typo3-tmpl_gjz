6a0e857af7cad19faf30c4608a83ddaf3308e9f5
mh-goe <marcus.hellmann.bsc@gmail.com>
2017-02-28 17:57:23 +0100

    Added chart (test)

---
+++ Classes/ViewHelpers/SystemstellenErschienenErschlossenViewHelper.php
@@ -0,0 +1,149 @@
+<?php
+/*******************************************************************************
+ * Copyright notice
+ *
+ * Copyright 2015 Marcus Hellmann, Göttingen Academy of Sciences and Humanities
+ *                <mhellma1@gwdg.de>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ ******************************************************************************/
+ 
+ 
+/**
+ * View Helper to return publication period of journal and the amount of records for each year
+ * optimized for visualization with jQuery.flot
+ */
+namespace Gjz18\TmplGjz\ViewHelpers;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+class SystemstellenErschienenErschlossenViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper {
+
+
+  /**
+   * Registers own arguments.
+   */
+  public function initializeArguments() {
+    parent::initializeArguments();
+    $this->registerArgument('beginn', 'int', 'The string to extract the year of publication begin from', TRUE);
+    $this->registerArgument('ende', 'int', 'The string to extract the year of publication end from', TRUE);
+  }
+
+  /**
+   * @return array
+   */
+  public function render() {
+    $result = NULL;
+    $finalString = NULL;
+    $xml = NULL;
+    $sxe = NULL;
+    $path = NULL;
+    $pubBegin = NULL;
+    $pubEnd = NULL;
+    $years = NULL;
+    $zssArrayRaw_Jahr = NULL;
+    $zssArrayRaw_Anzahl = NULL;
+    $zssArray_Jahr = NULL;
+    $zssArray_Anzahl = NULL;
+    
+    /** @years : length of publication period */
+    if ($this->arguments['beginn']) {
+      $pubBegin = $this->arguments['beginn'];
+    }
+    if ($this->arguments['ende']) {
+      $pubEnd = $this->arguments['ende'];
+    }
+    $years = ($pubEnd - $pubBegin) + 1;
+    
+    
+    
+    $url = "http://gjz18solr.tc.sub.uni-goettingen.de/solr410-adw/adw/select?q=text%3A*&rows=1&fl=i011_sa&wt=xml&indent=true&facet=true&facet.query=text%3A*&facet.field=i011_sa&facet.limit=150";
+    $includeHeader=false;
+    $requestHeaders=false;
+    $report=NULL;
+    
+    $xml = GeneralUtility::getUrl($url, $includeHeader, $requestHeaders, $report);
+    
+    
+    
+    /** Get XML content from solr answer */
+      if ($xml) {
+      
+        $sxe = new \SimpleXMLElement($xml);
+        
+        if ($sxe) {
+          $zssArrayRaw_Jahr = $sxe->xpath("//lst[@name='i011_sa']/int/@name");
+          $zssArrayRaw_Anzahl = $sxe->xpath("//lst[@name='i011_sa']/int");
+          $zssArray_Jahr = $zssArrayRaw_Jahr;
+          $zssArray_Anzahl = $zssArrayRaw_Anzahl;
+          
+          if ($zssArray_Anzahl[0] == "0") {
+            for ($year=$pubBegin; $year<($pubBegin+$years); $year++) {
+              if ($year!=$pubBegin) {
+                $finalString .= ", ";
+              }
+              $finalString .= "{data: [ ['".$year."', 11111] ], color: '#BBBBBB'}";
+            }
+          }
+          else {
+            for ($year=$pubBegin; $year<($pubBegin+$years); $year++) {
+              if ($year!=$pubBegin) {
+                $finalString .= ", ";
+              }
+              $keyJahrAnzahl = array_search($year, $zssArray_Jahr);
+              
+              if ( ($zssArray_Anzahl[$keyJahrAnzahl] == "0") OR ($keyJahrAnzahl===FALSE) ) {
+                $finalString .= "{data: [ ['".$year."', 11111] ], color: '#BBBBBB'}";
+              } else {
+                  /** Anzahl der Einträge pro Jahr **/
+                  $finalString .= "{data: [ ['".$year."', ".$zssArray_Anzahl[$keyJahrAnzahl]."] ], color: '#4579B3'}";
+              }
+            }
+          }
+          
+          /** einmaliges Labelling: "Erschlossen" **/
+          if (strpos($finalString, "color: '#4579B3'") !== false) {
+            $insertPos = strpos($finalString, "color: '#4579B3'")+16;
+            $first = (string)substr($finalString, 0, $insertPos);
+            $insert = ", label: 'Erschlossen'";
+            $last = (string)substr($finalString, $insertPos);
+            $finalString = $first.$insert.$last;
+          }
+          
+          /** einmaliges Labelling: "keine Einträge" **/
+          if (strpos($finalString, "color: '#BBBBBB'") !== false) {
+            $insertPos = strpos($finalString, "color: '#BBBBBB'")+16;
+            $first = (string)substr($finalString, 0, $insertPos);
+            $insert = ", label: 'keine Einträge'";
+            $last = (string)substr($finalString, $insertPos);
+            $finalString = $first.$insert.$last;
+          }
+          
+          //echo($finalString);
+          return $finalString;
+          
+        }
+        else return NULL;
+        
+      }
+      else return NULL;
+      
+  }
+  
+}
+
+?>
---
+++ Classes/ViewHelpers/SystemstellenFaecherViewHelper.php
@@ -0,0 +1,128 @@
+<?php
+/*******************************************************************************
+ * Copyright notice
+ *
+ * Copyright 2015 Marcus Hellmann, Göttingen Academy of Sciences and Humanities
+ *                <mhellma1@gwdg.de>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ ******************************************************************************/
+ 
+ 
+/**
+ * View Helper to return array of classification and frequencies
+ */
+namespace Gjz18\TmplGjz\ViewHelpers;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
+class SystemstellenFaecherViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper {
+
+
+  /**
+   * Registers own arguments.
+   */
+  public function initializeArguments() {
+    parent::initializeArguments();
+    $this->registerArgument('decision', 'integer', 'The array to extract the title from', TRUE);
+    $this->registerArgument('beginn', 'int', 'The int to extract the year of publication begin from');
+    $this->registerArgument('ende', 'int', 'The int to extract the year of publication end from');
+  }
+
+
+  /**
+   * @return array
+   */
+  public function render() {
+
+    $url = NULL;
+    $includeHeader = false;
+    $requestHeaders = false;
+    $report = NULL;
+    $xml = NULL;
+    $sxe = NULL;
+    $faecherCountArray = array();
+    
+    /** @years : length of publication period */
+    if ($this->arguments['beginn']) {
+      $pubBegin = $this->arguments['beginn'];
+    }
+    if ($this->arguments['ende']) {
+      $pubEnd = $this->arguments['ende'];
+    }
+    $years = ($pubEnd - $pubBegin) + 1;
+    
+    $faecher = array("WISSENSCHAFTSKUNDE", "PHILOLOGIE", "THEOLOGIE", "JURISPRUDENZ", "MEDIZIN", "PHILOSOPHIE", "P*DAGOGIK", "STAATSWISSENSCHAFTEN", "KRIEGSWISSENSCHAFTEN", "NATURKUNDE", "TECHNOLOGIE", "MATHEMATIK", "GEOGRAPHIE", "GESCHICHTE", "BILDENDE", "LITER*R", "VERMISCHTE");
+    $faecherClean = array("Wissenschaftskunde", "Philologie", "Theologie", "Jurisprudenz", "Medizin und Pharmazie", "Philosophie", "Pädagogik", "Staatswissenschaften", "Kriegswissenschaften", "Naturkunde", "Technologie und Gewerbekunde", "Mathematik", "Geographie", "Geschichte", "Bildende Kunst, schöne Literatur, Musik", "Literär-/Gelehrtengeschichte", "Vermischte Schriften");
+    $faecherCleanAbbrevs = array("Wiss.", "Philol.", "Theol.", "Jur.", "Med.", "Phil.", "Päd.", "Pol.", "Mil.", "Nat.", "Tech.", "Math.", "Geo.", "Gesch.", "Kü.", "Lit.", "Verm.");
+    $faecherCountArrayDim2 = array_fill(0, 16, "");
+    $faecherCountArrayDim3 = array_fill(0, $years, "");
+    
+    for ($y=0; $y<$years; $y++) {
+      
+      $year=$pubBegin+$y;
+      
+      for ($i=0; $i<count($faecher); $i++) {
+        $url = "http://gjz18solr.tc.sub.uni-goettingen.de/solr410-adw/adw/select?q=text%3A*&fq=i011_sa%3A%22".$year."%22+d045Q01sa%3A".$faecher[$i]."*&rows=0&fl=d045Q01sa&wt=xml&indent=true";
+        $xml = GeneralUtility::getUrl($url, $includeHeader, $requestHeaders, $report);
+        if ($xml==NULL) {
+          $fachCount[0] = 0;
+        }
+        else {
+          $sxe = new \SimpleXMLElement($xml);
+          $fachCount = $sxe->xpath("//response/result[@name='response']/@numFound");
+        }
+        $faecherCountArray[$i] = $fachCount[0];
+      }
+      
+      for ($c=0; $c<17; $c++) {
+        $faecherCountArrayDim2[$c] = [ $faecherCleanAbbrevs[$c], $faecherCountArray[$c] ];
+      }
+      
+      $faecherCountArrayDim3[$y] = $faecherCountArrayDim2;
+      
+    }
+    
+    
+    $strArr = "";
+    for ($bar=0; $bar<$years; $bar++) {
+      if ($bar!=0) {
+        $strArr .= ", ";
+      }
+      for ($foo=0; $foo<17; $foo++) {
+        if ($foo==0) {
+          $strArr .= "[ ";
+        }
+        $strArr .= "['".($faecherCountArrayDim3[$bar][$foo][0]."', ".$faecherCountArrayDim3[$bar][$foo][1]."]");
+        if ($foo==16) {
+          $strArr .= " ]";
+        }
+        else {
+          $strArr .= ", ";
+        }
+      }
+    }
+    
+    return $strArr;
+    //return $faecherCountArrayDim3;
+    
+  }
+  
+}
+
+?>
