6e14836453a4b91183ed43b035d2304a45445420
Marcus Hellmann <marcus.hellmann.bsc@gmail.com>
2017-10-18 15:42:33 +0200

    Revert "Extended detailed presentation of SWW"
    This reverts commit 4128adf1da03c7522bd201fe8f639831f7a461fa.

--- Classes/ViewHelpers/SwwSlashPrefixViewHelper.php
+++ Classes/ViewHelpers/SwwSlashPrefixViewHelper.php
@@ -67,9 +67,7 @@ class SwwSlashPrefixViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelper\Abstract
 						return $prefix;
 					}
 					elseif ( strpos($resultPt2, "/") !== FALSE ) {
-						$prefixPt1 = (string)substr($result, 0, strpos($result, $resultPt2));
-						$prefixPt2 = (string)substr($resultPt2, 0, strpos($resultPt2, "/"));
-						$prefix = $prefixPt1.$prefixPt2;
+						$prefix = (string)substr($result, 0, strpos($result, $resultPt2));
 						return $prefix;
 					}
 					else {
