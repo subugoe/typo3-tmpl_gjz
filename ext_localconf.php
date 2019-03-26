59863cccf1eeb4d66a684ef86f28bdfb58365897
mh-goe <marcus.hellmann.bsc@googlemail.com>
2019-03-26 10:01:51 +0100

    Minor bugfixes

--- ext_localconf.php
+++ ext_localconf.php
@@ -2,4 +2,4 @@
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT:source="FILE:EXT:tmpl_gjz/Configuration/PageTS/pageTsConfig.typoscript">');
 
-$GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['gjzpreset'] = 'EXT:tmpl_gjz/Configuration/RTE/GjzPreset.yaml';
+$GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['CustomPresets'] = 'typo3conf/ext/tmpl_gjz/Configuration/RTE/GjzPreset.yaml';
