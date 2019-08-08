4b3c57742e804c94ff2b27d59549f5b68e3e5c6e
Marcus Hellmann <hellmann@sub.uni-goettingen.de>
2019-08-08 16:30:38 +0200

    Remove duplicate values from "published" arrays
    This solves the rendering problem described in ticket 28840.
    Nevertheless the base records should be updated one day.

---
+++ Classes/ViewHelpers/ArrayUniqueViewHelper.php
@@ -0,0 +1,65 @@
+<?php
+/*******************************************************************************
+ * Copyright notice
+ *
+ * Copyright 2013 Sven-S. Porst, Göttingen State and University Library
+ *                <porst@sub.uni-goettingen.de>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ ******************************************************************************/
+
+namespace Gjz18\TmplGjz\ViewHelpers;
+
+use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper;
+
+
+/**
+ * View Helper to return the the passed array without duplicates.
+ */
+class ArrayUniqueViewHelper extends AbstractViewHelper {
+
+
+	/**
+	 * Registers own arguments.
+	 */
+	public function initializeArguments() {
+		parent::initializeArguments();
+		$this->registerArgument('array', 'array', 'the array to eliminate duplicates from', TRUE);
+	}
+
+
+	/**
+	 * @return array
+	 */
+	public function render() {
+		$result = NULL;
+
+		if (is_array($this->arguments['array']) && count($this->arguments['array']) > 0) {
+			$result = $this->arguments['array'];
+		}
+
+		$result = array_unique($result);
+		$result = array_values($result);
+
+		return $result;
+	}
+
+}
+
+?>
