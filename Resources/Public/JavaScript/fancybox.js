ecca6555b03951a59aea5e14ade49b264474943e
Marcus Hellmann <hellmann@sub.uni-goettingen.de>
2019-11-25 10:19:32 +0100

    Cleanup code

--- Resources/Public/JavaScript/fancybox.js
+++ Resources/Public/JavaScript/fancybox.js
@@ -1,23 +1,19 @@
-<script type="text/javascript">
-
 // Image
 $("a.lightbox").fancybox({
-  'zoomSpeedIn': 300,
-  'zoomSpeedOut': 300,
-  'overlayShow': true
+  "zoomSpeedIn": 300,
+  "zoomSpeedOut": 300,
+  "overlayShow": true
  });
 
 // iFrame Content
 $("a.lightbox-page").fancybox({
-  'zoomSpeedIn': 300,
-  'zoomSpeedOut': 300,
-  'overlayShow': true,
-  'width': 650,
-  'height': 280,
-  'titleShow': false,
-  'autoScale' : true,
-  'type': 'iframe'
+  "zoomSpeedIn": 300,
+  "zoomSpeedOut": 300,
+  "overlayShow": true,
+  "width": 650,
+  "height": 280,
+  "titleShow": false,
+  "autoScale" : true,
+  "type": "iframe"
  });
-
-</script>
 
