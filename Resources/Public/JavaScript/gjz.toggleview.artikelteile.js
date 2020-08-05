da5e728aa6e7753c877d64941f8190bef4e1066f
Marcus Hellmann <hellmann@sub.uni-goettingen.de>
2020-08-05 20:26:08 +0200

    Change jQuery click function call and use aliases

--- Resources/Public/JavaScript/gjz.toggleview.artikelteile.js
+++ Resources/Public/JavaScript/gjz.toggleview.artikelteile.js
@@ -1,37 +1,40 @@
 /* Toggle View Artikelteile */
-jQuery(document).ready(function() {
-	var list = jQuery("#liste-teilartikel");
-	var sizeList = jQuery(list).children().length;
-	var currentSelection = jQuery(".current-artikelteil");
-	var indexPositionCurrent = jQuery(list).children().index(currentSelection);
+$(function() {
+	var list = $("#liste-teilartikel");
+	var sizeList = $(list).children().length;
+	var currentSelection = $(".current-artikelteil");
+	var indexPositionCurrent = $(list).children().index(currentSelection);
 
 	if (sizeList > 7) {
-		jQuery("#toggle-list-artikelteile").show();
+		$("#toggle-list-artikelteile").show();
 
 		if (indexPositionCurrent <= 2) {
-			jQuery(list).children().slice(5, sizeList).hide();
+			$(list).children().slice(5, sizeList).hide();
 		} else if (indexPositionCurrent >= sizeList-2) {
-			jQuery(list).children().slice(0, sizeList-5).hide();
+			$(list).children().slice(0, sizeList-5).hide();
 		} else{
-			jQuery(list).children().slice(0, indexPositionCurrent-2).hide();
-			jQuery(list).children().slice(indexPositionCurrent+3, sizeList).hide();
+			$(list).children().slice(0, indexPositionCurrent-2).hide();
+			$(list).children().slice(indexPositionCurrent+3, sizeList).hide();
 		}
 
-		jQuery("#toggle-list-artikelteile").on("click", function(e) {
-			if ( jQuery(list).hasClass("collapsed") ) {
+		$("#toggle-list-artikelteile").click(function(e) {
+			if ( $(list).hasClass("collapsed") ) {
 				e.preventDefault();
-				jQuery(list).children().slice(0, indexPositionCurrent-2).show();
-				jQuery(list).children().slice(indexPositionCurrent+3, sizeList).show();
-				jQuery(list).removeClass("collapsed");
-				jQuery("#toggle-list-artikelteile").removeClass("collapsed").addClass("expanded").html("Liste einklappen");
+				$(list).children().show();
+				$(list).removeClass("collapsed");
+				$("#toggle-list-artikelteile").removeClass("collapsed").addClass("expanded").html("Liste einklappen");
 			} else {
 				e.preventDefault();
-				if (indexPositionCurrent >= 2) {
-					jQuery(list).children().slice(0, indexPositionCurrent-2).hide();
+				if (indexPositionCurrent <= 2) {
+					$(list).children().slice(5, sizeList).hide();
+				} else if (indexPositionCurrent >= sizeList-2) {
+					$(list).children().slice(0, sizeList-5).hide();
+				} else{
+					$(list).children().slice(0, indexPositionCurrent-2).hide();
+					$(list).children().slice(indexPositionCurrent+3, sizeList).hide();
 				}
-				jQuery(list).children().slice(indexPositionCurrent+3, sizeList).hide();
-				jQuery(list).addClass("collapsed");
-				jQuery("#toggle-list-artikelteile").removeClass("expanded").addClass("collapsed").html("Liste ausklappen");
+				$(list).addClass("collapsed");
+				$("#toggle-list-artikelteile").removeClass("expanded").addClass("collapsed").html("Liste ausklappen");
 			}
 		});
 	}
